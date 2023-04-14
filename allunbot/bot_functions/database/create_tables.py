682b2a8e449bb159d05a1a3bac10a93d55becc5c
David Pérez <dperezz@unal.edu.co>
2023-04-13 23:37:21 -0500

    Updated: deleted some temp files

--- allunbot/bot_functions/database/create_tables.py
+++
@@ -1,7 +0,0 @@
-from ..bot_fuctions.academic_history import *
-from ..bot_fuctions.metrics import *
-from ..bot_fuctions.login import *
-
-create_table_users()
-create_table_metrics()
-create_table_academic_history()
